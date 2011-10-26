96f448cf53930748c00c0beeb0e43f6c1312523f
Francis <francismpp@gmail.com>
2011-10-26 02:13:51 +0200

    Added seeds that copy categories and products in ceramicapadilla.es

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,87 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: utf-8
+
+require 'ffaker'
+
+cazuelas = Category.create!(
+  :name => "Cazuelas",
+  :description => Faker::Lorem.paragraphs(2).join("\n\n"),
+  :remote_picture_url => "http://ceramicapadilla.es/cazuelas/images/dpp_0001.jpg"
+)
+
+1.upto(7) do |i|
+  cazuelas.products.create!(
+    :name => Faker::Lorem.sentence(3),
+    :description => Faker::Lorem.paragraphs(2).join("\n\n"),
+    :remote_picture_url => "http://ceramicapadilla.es/cazuelas/images/dpp_000#{i}.jpg"
+  )
+end
+
+jardineria = Category.create!(
+  :name => "Jardinería",
+  :description => Faker::Lorem.paragraphs(2).join("\n\n"),
+  :remote_picture_url => "http://ceramicapadilla.es/jardin/images/067.jpg"
+)
+
+%w(
+  067
+  078
+  088
+  099
+  110
+  118
+  119
+  120
+  121
+  068
+  069
+  070
+  071
+  072
+  073
+  074
+  075
+  076
+  077
+  079
+  080
+  081
+  082
+  083
+  084
+  085
+  086
+  087
+  089
+  090
+  091
+  092
+  093
+  094
+  095
+  096
+  097
+  098
+  100
+  101
+  102
+  103
+  104
+  105
+  106
+  107
+  109
+  108
+  111
+  112
+  113
+  114
+  115
+  116
+  117
+).each do |i|
+  jardineria.products.create!(
+    :name => Faker::Lorem.sentence(3),
+    :description => Faker::Lorem.paragraphs(2).join("\n\n"),
+    :remote_picture_url => "http://ceramicapadilla.es/jardin/images/#{i}.jpg"
+  )
+end
