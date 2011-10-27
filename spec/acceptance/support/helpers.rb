99f659bb1819aca674b2cf2368c442bf141df59d
Francis <f.perez@flowlab.biz>
2011-10-27 12:45:18 +0200

    Fixed indentation

--- spec/acceptance/support/helpers.rb
+++ spec/acceptance/support/helpers.rb
@@ -9,12 +9,12 @@ module HelperMethods
   end
 end
 
-  module Capybara
-    class Session
-      def has_image?(src)
-        has_xpath?("//img[contains(@src,\"/#{src}\")]")
-      end
+module Capybara
+  class Session
+    def has_image?(src)
+      has_xpath?("//img[contains(@src,\"/#{src}\")]")
     end
   end
+end
 
 RSpec.configuration.include HelperMethods, :type => :acceptance
