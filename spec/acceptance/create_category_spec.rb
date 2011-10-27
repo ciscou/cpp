cdf1d5b8f8fba2196cfcddf62c09d74e8c580b2a
Francis <f.perez@flowlab.biz>
2011-10-27 12:45:42 +0200

    Added acceptance test for not admin users trying to create categories and products

--- spec/acceptance/create_category_spec.rb
+++ spec/acceptance/create_category_spec.rb
@@ -8,35 +8,72 @@ feature 'Create category', %q{
   I want to create categories first
 } do
 
-  scenario 'with valid attributes' do
-    admin = Factory :user, :admin => true
-    login_as admin
-
-    click_link "Catálogo"
-
-    page.should have_no_css ".category"
-
-    click_link "Añadir otra categoría"
-
-    fill_in "Nombre", :with => "Now this is an awesome category!"
-    fill_in "Descripción", :with => "This category will be soon full of awesome products.\n\nJust come back in a few minutes"
-    attach_file "Foto", Rails.root.join("spec", "acceptance", "data", "example_picture.jpg")
-    click_button "Guardar"
-
-    within ".category" do
-      page.should have_css "h2", :text => "Now this is an awesome category!"
-      page.should have_content "This category will be soon full of awesome products."
-      page.should have_content "Just come back in a few minutes"
-      page.should have_image "now-this-is-an-awesome-category.jpg"
+  context "as guest" do
+    background do
+      visit homepage
     end
 
-    click_link "Catálogo"
+    scenario "should not see link" do
+      click_link "Catálogo"
+      page.should have_no_link "Añadir otra categoría"
+    end
 
-    within ".category" do
-      page.should have_css "h2", :text => "Now this is an awesome category!"
-      page.should have_content "This category will be soon full of awesome products."
-      page.should have_content "Just come back in a few minutes"
-      page.should have_image "thumb_now-this-is-an-awesome-category.jpg"
+    scenario "should not be able to see the form" do
+      visit new_category_url
+      current_url.should == root_url
+    end
+  end
+
+  context "as regular user" do
+    background do
+      user = Factory :user, :admin => false
+      login_as user
+    end
+
+    scenario "should not see link" do
+      click_link "Catálogo"
+      page.should have_no_link "Añadir otra categoría"
+    end
+
+    scenario "should not be able to see the form" do
+      visit new_category_url
+      current_url.should == root_url
+    end
+  end
+
+  context "as admin" do
+    background do
+      admin = Factory :user, :admin => true
+      login_as admin
+    end
+
+    scenario 'with valid attributes' do
+      click_link "Catálogo"
+
+      page.should have_no_css ".category"
+
+      click_link "Añadir otra categoría"
+
+      fill_in "Nombre", :with => "Now this is an awesome category!"
+      fill_in "Descripción", :with => "This category will be soon full of awesome products.\n\nJust come back in a few minutes"
+      attach_file "Foto", Rails.root.join("spec", "acceptance", "data", "example_picture.jpg")
+      click_button "Guardar"
+
+      within ".category" do
+        page.should have_css "h2", :text => "Now this is an awesome category!"
+        page.should have_content "This category will be soon full of awesome products."
+        page.should have_content "Just come back in a few minutes"
+        page.should have_image "now-this-is-an-awesome-category.jpg"
+      end
+
+      click_link "Catálogo"
+
+      within ".category" do
+        page.should have_css "h2", :text => "Now this is an awesome category!"
+        page.should have_content "This category will be soon full of awesome products."
+        page.should have_content "Just come back in a few minutes"
+        page.should have_image "thumb_now-this-is-an-awesome-category.jpg"
+      end
     end
   end
 
--- spec/acceptance/create_product_spec.rb
+++ spec/acceptance/create_product_spec.rb
@@ -8,37 +8,76 @@ feature 'Create product', %q{
   I want to create a product in a category
 } do
 
-  scenario 'with valid attributes' do
-    admin    = Factory :user, :admin => true
-    category = Factory :category
+  background do
+    @category = Factory :category
+  end
 
-    login_as admin
-
-    click_link category.name
-
-    page.should have_no_css ".product"
-
-    click_link "Añadir otro producto a esta categoría"
-
-    fill_in "Nombre", :with => "Now this is an awesome product!"
-    fill_in "Descripción", :with => "This is the most awesome product yet in this category.\n\nNot only because this is the only one"
-    attach_file "Foto", Rails.root.join("spec", "acceptance", "data", "example_picture.jpg")
-    click_button "Guardar"
-
-    within ".product" do
-      page.should have_css "h2", :text => "Now this is an awesome product!"
-      page.should have_content "This is the most awesome product yet in this category."
-      page.should have_content "Not only because this is the only one"
-      page.should have_image "now-this-is-an-awesome-product.jpg"
+  context "as guest" do
+    background do
+      visit homepage
     end
 
-    click_link category.name
+    scenario "should not see link" do
+      click_link @category.name
+      page.should have_no_link "Añadir otro producto a esta categoría"
+    end
 
-    within ".product" do
-      page.should have_css "h2", :text => "Now this is an awesome product!"
-      page.should have_content "This is the most awesome product yet in this category."
-      page.should have_content "Not only because this is the only one"
-      page.should have_image "thumb_now-this-is-an-awesome-product.jpg"
+    scenario "should not be able to see the form" do
+      visit new_category_product_url(@category)
+      current_url.should == root_url
+    end
+  end
+
+  context "as regular user" do
+    background do
+      user = Factory :user, :admin => false
+      login_as user
+    end
+
+    scenario "should not see link" do
+      click_link @category.name
+      page.should have_no_link "Añadir otro producto a esta categoría"
+    end
+
+    scenario "should not be able to see the form" do
+      visit new_category_product_url(@category)
+      current_url.should == root_url
+    end
+  end
+
+  context "as admin" do
+    background do
+      admin = Factory :user, :admin => true
+      login_as admin
+    end
+
+    scenario 'with valid attributes' do
+      click_link @category.name
+
+      page.should have_no_css ".product"
+
+      click_link "Añadir otro producto a esta categoría"
+
+      fill_in "Nombre", :with => "Now this is an awesome product!"
+      fill_in "Descripción", :with => "This is the most awesome product yet in this category.\n\nNot only because this is the only one"
+      attach_file "Foto", Rails.root.join("spec", "acceptance", "data", "example_picture.jpg")
+      click_button "Guardar"
+
+      within ".product" do
+        page.should have_css "h2", :text => "Now this is an awesome product!"
+        page.should have_content "This is the most awesome product yet in this category."
+        page.should have_content "Not only because this is the only one"
+        page.should have_image "now-this-is-an-awesome-product.jpg"
+      end
+
+      click_link @category.name
+
+      within ".product" do
+        page.should have_css "h2", :text => "Now this is an awesome product!"
+        page.should have_content "This is the most awesome product yet in this category."
+        page.should have_content "Not only because this is the only one"
+        page.should have_image "thumb_now-this-is-an-awesome-product.jpg"
+      end
     end
   end
 
