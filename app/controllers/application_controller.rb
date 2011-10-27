bdb2d652274c0f4397d5984fa6f8dc9d3c6a09a3
Francis <f.perez@flowlab.biz>
2011-10-27 11:58:13 +0200

    Redirect to root_url on authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   enable_authorization :unless => :devise_controller? do |exception|
-    redirect_to new_user_session_path, :alert => exception.message
+    redirect_to root_url, :alert => exception.message
   end
 end
