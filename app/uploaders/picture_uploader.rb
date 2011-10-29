9a2c3bfdb6349b3ae53197f28162142ee3d1581e
Francis <francismpp@gmail.com>
2011-10-29 12:29:01 +0200

    Limit image heigth

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -23,7 +23,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [640, nil]
+  process :resize_to_limit => [640, 480]
 
   # def scale(width, height)
   #   # do something
