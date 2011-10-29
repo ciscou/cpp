326c1740aebc4cea5a8df801820574cbe2ce961a
Francis <francismpp@gmail.com>
2011-10-29 19:17:47 +0200

    Added some acceptance testing for the new roles

--- spec/acceptance/list_products_by_category_spec.rb
+++ spec/acceptance/list_products_by_category_spec.rb
@@ -6,16 +6,42 @@ feature 'List products by category', %q{
   I want to select a category in the menu
 } do
 
-  scenario 'with two products, one of them in selected category' do
-    product1 = Factory :product
-    product2 = Factory :product
+  background do
+    @product1 = Factory :product
+    @product2 = Factory :product
+    @product3 = Factory :product, :category => @product1.category, :new_arrival => true
+  end
 
+  scenario 'as an annonymous user' do
     visit homepage
 
-    click_link product1.category.name
+    click_link @product1.category.name
 
-    page.should have_css    ".product div.image-title", :text => product1.name
-    page.should have_no_css ".product div.image-title", :text => product2.name
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_no_css ".product div.image-title", :text => @product3.name
+  end
+
+  scenario 'as a regular user' do
+    user = Factory :user, :premium => false
+    login_as user
+
+    click_link @product1.category.name
+
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_no_css ".product div.image-title", :text => @product3.name
+  end
+
+  scenario 'as a premium user' do
+    user = Factory :user, :premium => true
+    login_as user
+
+    click_link @product1.category.name
+
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_css    ".product div.image-title", :text => @product3.name
   end
 
 end
--- spec/acceptance/search_products_spec.rb
+++ spec/acceptance/search_products_spec.rb
@@ -6,17 +6,45 @@ feature 'Search products', %q{
   I want to use the simple search form
 } do
 
-  scenario 'with two products, one of them matches search criteria' do
-    product1 = Factory :product, :name => "an awesome product"
-    product2 = Factory :product, :name => "another cool product"
+  background do
+    @product1 = Factory :product, :name => "an awesome product"
+    @product2 = Factory :product, :name => "another cool product"
+    @product3 = Factory :product, :name => "a new awesome product", :new_arrival => true
+  end
 
+  scenario 'as an annonymous user' do
     visit homepage
 
     fill_in "q", :with => "awesome product"
     click_button "Buscar"
 
-    page.should have_css    ".product div.image-title", :text => product1.name
-    page.should have_no_css ".product div.image-title", :text => product2.name
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_no_css ".product div.image-title", :text => @product3.name
+  end
+
+  scenario 'as a regular user' do
+    user = Factory :user, :premium => false
+    login_as user
+
+    fill_in "q", :with => "awesome product"
+    click_button "Buscar"
+
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_no_css ".product div.image-title", :text => @product3.name
+  end
+
+  scenario 'as a premium user' do
+    user = Factory :user, :premium => true
+    login_as user
+
+    fill_in "q", :with => "awesome product"
+    click_button "Buscar"
+
+    page.should have_css    ".product div.image-title", :text => @product1.name
+    page.should have_no_css ".product div.image-title", :text => @product2.name
+    page.should have_css    ".product div.image-title", :text => @product3.name
   end
 
 end
