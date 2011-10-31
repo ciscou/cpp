292f64ae2de93dfc3b5f38efb4202b390e6809fe
Francis <f.perez@flowlab.biz>
2011-10-31 15:19:27 +0100

    Updated jquery.history (had to hack a little jquery.galleriffic)

--- app/assets/javascripts/galleriffic.js
+++ app/assets/javascripts/galleriffic.js
@@ -1,89 +1,91 @@
 jQuery(document).ready(function($) {
 
-  // We only want these styles applied when javascript is enabled
-  $('div.navigation').css({'width' : '300px', 'float' : 'left'});
-  $('div.galleriffic').css('display', 'block');
+  if($("#thumbs").length > 0) {
+    // We only want these styles applied when javascript is enabled
+    $('div.navigation').css({'width' : '300px', 'float' : 'left'});
+    $('div.galleriffic').css('display', 'block');
 
-  // Initially set opacity on thumbs and add
-  // additional styling for hover effect on thumbs
-  var onMouseOutOpacity = 0.67;
-  $('#thumbs ul.thumbs li').opacityrollover({
-    mouseOutOpacity:   onMouseOutOpacity,
-    mouseOverOpacity:  1.0,
-    fadeSpeed:         'fast',
-    exemptionSelector: '.selected'
-  });
+    // Initially set opacity on thumbs and add
+    // additional styling for hover effect on thumbs
+    var onMouseOutOpacity = 0.67;
+    $('#thumbs ul.thumbs li').opacityrollover({
+      mouseOutOpacity:   onMouseOutOpacity,
+      mouseOverOpacity:  1.0,
+      fadeSpeed:         'fast',
+      exemptionSelector: '.selected'
+    });
 
-  // Initialize Advanced Galleriffic Gallery
-  var gallery = $('#thumbs').galleriffic({
-    delay:                     5000,
-    numThumbs:                 8,
-    preloadAhead:              10,
-    enableTopPager:            true,
-    enableBottomPager:         true,
-    maxPagesToShow:            7,
-    imageContainerSel:         '#slideshow',
-    controlsContainerSel:      '#controls',
-    captionContainerSel:       '#caption',
-    loadingContainerSel:       '#loading',
-    renderSSControls:          true,
-    renderNavControls:         true,
-    playLinkText:              'Ver Presentación',
-    pauseLinkText:             'Detener Presentación',
-    prevLinkText:              '&lsaquo; Anterior foto',
-    nextLinkText:              'Siguiente foto &rsaquo;',
-    nextPageLinkText:          'Siguiente &rsaquo;',
-    prevPageLinkText:          '&lsaquo; Anterior',
-    enableHistory:             true,
-    autoStart:                 false,
-    syncTransitions:           true,
-    defaultTransitionDuration: 900,
-    onSlideChange:             function(prevIndex, nextIndex) {
-      // 'this' refers to the gallery, which is an extension of $('#thumbs')
-      this.find('ul.thumbs').children()
-        .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
-        .eq(nextIndex).fadeTo('fast', 1.0);
-    },
-    onPageTransitionOut:       function(callback) {
-      this.fadeTo('fast', 0.0, callback);
-    },
-    onPageTransitionIn:        function() {
-      this.fadeTo('fast', 1.0);
+    // Initialize Advanced Galleriffic Gallery
+    var gallery = $('#thumbs').galleriffic({
+      delay:                     5000,
+      numThumbs:                 8,
+      preloadAhead:              10,
+      enableTopPager:            true,
+      enableBottomPager:         true,
+      maxPagesToShow:            7,
+      imageContainerSel:         '#slideshow',
+      controlsContainerSel:      '#controls',
+      captionContainerSel:       '#caption',
+      loadingContainerSel:       '#loading',
+      renderSSControls:          true,
+      renderNavControls:         true,
+      playLinkText:              'Ver Presentación',
+      pauseLinkText:             'Detener Presentación',
+      prevLinkText:              '&lsaquo; Anterior foto',
+      nextLinkText:              'Siguiente foto &rsaquo;',
+      nextPageLinkText:          'Siguiente &rsaquo;',
+      prevPageLinkText:          '&lsaquo; Anterior',
+      enableHistory:             true,
+      autoStart:                 false,
+      syncTransitions:           true,
+      defaultTransitionDuration: 900,
+      onSlideChange:             function(prevIndex, nextIndex) {
+        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+        this.find('ul.thumbs').children()
+          .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+          .eq(nextIndex).fadeTo('fast', 1.0);
+      },
+      onPageTransitionOut:       function(callback) {
+        this.fadeTo('fast', 0.0, callback);
+      },
+      onPageTransitionIn:        function() {
+        this.fadeTo('fast', 1.0);
+      }
+    });
+
+    // PageLoad function
+    // This function is called when:
+    // 1. after calling $.history.init();
+    // 2. after calling $.history.load();
+    // 3. after pushing "Go Back" button of a browser
+    function pageload(hash) {
+      // alert("pageload: " + hash);
+      // hash doesn't contain the first # character.
+      if(hash) {
+        $.galleriffic.gotoImage(hash);
+      } else {
+        gallery.gotoIndex(0);
+      }
     }
-  });
 
-  // PageLoad function
-  // This function is called when:
-  // 1. after calling $.historyInit();
-  // 2. after calling $.historyLoad();
-  // 3. after pushing "Go Back" button of a browser
-  function pageload(hash) {
-    // alert("pageload: " + hash);
-    // hash doesn't contain the first # character.
-    if(hash) {
-      $.galleriffic.gotoImage(hash);
-    } else {
-      gallery.gotoIndex(0);
-    }
+    // Initialize history plugin.
+    // The callback is called at once by present location.hash.
+    $.history.init(pageload);
+
+    // set onlick event for buttons using the jQuery 1.3 live method
+    $("a[rel='history']").live('click', function(e) {
+      if (e.button != 0) return true;
+
+      var hash = this.href;
+      hash = hash.replace(/^.*#/, '');
+
+      // moves to a new page. 
+      // pageload is called at once. 
+      // hash don't contain "#", "?"
+      $.history.load(hash);
+
+      return false;
+    });
   }
-
-  // Initialize history plugin.
-  // The callback is called at once by present location.hash.
-  $.historyInit(pageload);
-
-  // set onlick event for buttons using the jQuery 1.3 live method
-  $("a[rel='history']").live('click', function(e) {
-    if (e.button != 0) return true;
-
-    var hash = this.href;
-    hash = hash.replace(/^.*#/, '');
-
-    // moves to a new page. 
-    // pageload is called at once. 
-    // hash don't contain "#", "?"
-    $.historyLoad(hash);
-
-    return false;
-  });
 
 });
