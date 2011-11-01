0c0bb2d3a0fd0698e23e9ac800d8188b36b85e70
Francis <francismpp@gmail.com>
2011-11-01 12:18:38 +0100

    Made listings images bigger

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -30,6 +30,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
+
+  version :small do
+    process :resize_to_limit => [240, 180]
+  end
+
   version :thumb do
     process :resize_to_fill => [128, 128]
   end
