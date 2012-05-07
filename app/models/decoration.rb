1d4d81f610cf69f3f02cfb5f3b06cd87b962dc79
Francis <francis@aspgems.com>
2012-05-07 20:21:56 +0200

    updated list of decorations

--- app/models/decoration.rb
+++ app/models/decoration.rb
@@ -1,9 +1,24 @@
+#encoding: utf-8
+
 class Decoration
   DECORATIONS = {
-    "bla" => "Bla",
-    "ble" => "Ble",
-    "bli" => "Bli",
-    "blo" => "Blo"
+    "popular"              => "Popular",
+    "classic"              => "Clásico",
+    "228"                  => "228",
+    "207"                  => "207",
+    "carmen"               => "Carmen",
+    "honey_c1"             => "Miel C1",
+    "figs"                 => "Higos",
+    "olive_tree_r"         => "Oliva R",
+    "222"                  => "222",
+    "smoot_brushed_colors" => "Colores pincelados lisos",
+    "blue_alhambra"        => "Alhambra azul",
+    "green_alhambra"       => "Alhambra verde",
+    "blue_retro"           => "Retro azul",
+    "green_retro"          => "Retro verde",
+    "olive_r"              => "Aceituna R",
+    "baecula"              => "Baécula",
+    "multi_flower"         => "Multiflor"
   }
 
   def self.codes_to_mask(codes)
