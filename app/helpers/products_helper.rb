3a8697cabea59db43e167bf03b764347b641936c
Francis <francis@aspgems.com>
2012-05-13 12:59:19 +0200

    changed highlighted searches

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module ProductsHelper
   def new_arrivals_path
     if can? :new_arrivals, :products
@@ -13,5 +15,10 @@ module ProductsHelper
     else
       pages_new_arrivals_url
     end
+  end
+
+  def link_to_search_products(query)
+    link_to "Búsqueda '#{query}' (#{Product.accessible_by(current_ability).search(query).count})",
+      search_products_path(:q => '#{query}')
   end
 end
