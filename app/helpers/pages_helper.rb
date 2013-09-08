aac32ee99d7d399b0048db8013238d647163b7b5
ciscou <francismpp@gmail.com>
2013-09-08 20:47:36 +0200

    added more images to the how we work section

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def how_we_work_images
+    (0..9).reject { |i| i.in?(2, 4) }.map { |i| "DPP_100#{i}" } +
+    (10..17).map { |i| "DPP_10#{i}" } +
+    %w[1 ANA JUANJO maria _MG_0390 _MG_0397 paqui TAMIA_1]
+  end
 end
