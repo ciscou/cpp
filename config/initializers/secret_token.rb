c360ff62a428a0a6d4539212de0196e9002fdc57
ciscou <francismpp@gmail.com>
2013-10-07 21:25:43 +0200

    keep secret token... errr... secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cpp::Application.config.secret_token = '3d1fadf40f8e37c12f9e13408eca085296d704c7d231a929d112a5af1ebfe552567230be9925b3d5dc41c66cfb6b0c0949cb91aa02f89a5020947c58dd7b1684'
+Cpp::Application.config.secret_token = ENV['SECRET_TOKEN'].presence || '3d1fadf40f8e37c12f9e13408eca085296d704c7d231a929d112a5af1ebfe552567230be9925b3d5dc41c66cfb6b0c0949cb91aa02f89a5020947c58dd7b1684'
