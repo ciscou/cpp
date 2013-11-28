7378b89c32f57b60f4b045260551298773400730
ciscou <francismpp@gmail.com>
2013-11-28 23:11:58 +0100

    release mobile version to the wild

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,8 +40,7 @@ class ApplicationController < ActionController::Base
     if session[:mobile_override]
       session[:mobile_override] == "1"
     else
-#     request.user_agent =~ /Mobile|webOS/
-      false
+      (request.user_agent =~ /Mobile|webOS/) && (request.user_agent !~ /iPad/)
     end
   end
   helper_method :mobile_device?
