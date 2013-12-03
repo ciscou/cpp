c42b32003addf73700be50f04216e71e4d3e1c56
ciscou <francismpp@gmail.com>
2013-12-03 21:21:54 +0100

    redone caching

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -1,25 +1,51 @@
 module CacheHelper
-  def sidebar_highlighted_categories_cache_key
-    latest_product_cache_key("sidebar_highlighted_categories")
+  def cache_home_featured_products(&block)
+    cpp_cache "home_featured_products", expires_in: 1.hour, race_condition_ttl: 10 do
+      block.call
+    end
   end
 
-  def sidebar_all_categories_cache_key
-    latest_category_cache_key("sidebar_all_categories")
+  def cache_category_new_arrivals(&block)
+    cpp_cache latest_product_cache_key("category_new_arrivals"), &block
   end
 
-  def sidebar_decorations_cache_key
-    latest_category_cache_key("sidebar_decorations")
+  def cache_categories_index(&block)
+    cpp_cache latest_product_cache_key("categories_index"), &block
   end
 
-  def new_arrivals_cache_key
-    latest_product_cache_key("new_arrivals")
+  def cache_category_products(category, &block)
+    cpp_cache category, &block
   end
 
-  def all_categories_cache_key
-    latest_category_cache_key("all_categories")
+  def cache_products_new_arrivals(&block)
+    cpp_cache latest_product_cache_key("products_new_arrivals"), &block
+  end
+
+  def cache_sidebar_highlighted_categories(&block)
+    cpp_cache latest_product_cache_key("sidebar_highlighted_categories"), &block
+  end
+
+  def cache_sidebar_all_categories(&block)
+    cpp_cache latest_category_cache_key("sidebar_all_categories"), &block
+  end
+
+  def cache_sidebar_decorations(&block)
+    cpp_cache latest_category_cache_key("sidebar_decorations"), &block
   end
 
   private
+
+  def cpp_cache(*keys, &block)
+    options = keys.extract_options!
+    keys.unshift(I18n.locale, mobile_device? ? :mobile : :desktop)
+
+    cache keys, options do
+      html = content_tag :div, class: "cache", data: { generated_at: Time.current.to_s(:db), key: keys.join("/") } do
+        block.call
+      end
+      safe_concat html
+    end
+  end
 
   def latest_product_cache_key(prefix)
     latest_model_cache_key(Product, prefix)
